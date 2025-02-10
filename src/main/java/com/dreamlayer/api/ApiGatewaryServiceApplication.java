8e3fc3edfabf6196a21d90cc60a6d5055dbe2e35
shandeepkumarIT <shandeepkumarbme@gmail.com>
2025-02-09 20:20:05 -0500

    api-gateway-service

---
+++ src/main/java/com/dreamlayer/api/ApiGatewaryServiceApplication.java
@@ -0,0 +1,15 @@
+package com.dreamlayer.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ApiGatewaryServiceApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewaryServiceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/dreamlayer/api/routes/Routes.java
@@ -0,0 +1,32 @@
+package com.dreamlayer.api.routes;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Routes {
+
+	@Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("product-service",
+                        r -> r.path("/api/product")
+                                .uri("lb://product-service"))
+                .route("order-service",
+                        r -> r.path("/api/order")
+                                .uri("lb://order-service"))
+                .route("inventory-service",
+                        r -> r.path("/api/inventory")
+                                .uri("lb://inventory-service"))
+                .route("discover-service",
+                        r -> r.path("/eureka/web")
+                        		.filters(a -> a.setPath("/"))
+                                .uri("http://localhost:8761"))
+                .route("discover-service-static",
+                        r -> r.path("/eureka/**")
+                                .uri("http://localhost:8761"))
+                .build();
+    }
+}
